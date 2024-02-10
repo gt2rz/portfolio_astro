b316e2e94ab40922abc3e4e133be9d5bcfa49f45
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-02-10 10:55:20 -0500

    Refactor animate_on_scroll.js to improve intersection observer threshold

--- src/scripts/animate_on_scroll.js
+++ src/scripts/animate_on_scroll.js
@@ -1,24 +1,24 @@
 const animation = document.querySelectorAll(".animate-on-scroll-section");
 
-const callback = () => {
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("animate-on-scroll");
-            } else {
-                entry.target.classList.remove("animate-on-scroll");
-            }
-        });
-    }
-}
+const callback = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("intersecting");
+            entry.target.classList.add("animate-on-scroll");
+        } else {
+            console.log("not intersecting");
+            entry.target.classList.remove("animate-on-scroll");
+        }
+    });
+};
 
 const options = {
     // root: null,
     // rootMargin: "0px",
-    threshold: 1.0,
+    threshold: 0.5,
 };
 
-const observer = new IntersectionObserver( callback, options );
+const observer = new IntersectionObserver(callback, options);
 
 for (let i = 0; i < animation.length; i++) {
     observer.observe(animation[i]);
